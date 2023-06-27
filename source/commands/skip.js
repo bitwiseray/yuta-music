c93e0a7834452b05844492c95f6c58126b6aea88
yesiamray <itzryan.777@gmail.com>
2023-06-27 18:03:49 +0530

    Added skip response

--- source/commands/skip.js
+++ source/commands/skip.js
@@ -8,6 +8,7 @@ module.exports = {
     async execute(interaction, yuta) {
         if (!interaction.member.voice.channel) return interaction.reply('You need to be in a voice channel.');
         let songQueue = yuta.queue.get(interaction.guild.id);
+        interaction.reply(`Skipped **${songQueue.songs[0]}**`);
         songQueue.songs.shift();
         streamPlayer(interaction.guild.id, songQueue.songs[0], yuta);
     },
