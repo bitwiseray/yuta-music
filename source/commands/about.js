3ed5d79b687388e87c474938bb9cfa35524cc4c9
yesiamray <itzryan.777@gmail.com>
2023-06-27 17:38:39 +0530

    Added new list-style markup

--- source/commands/about.js
+++ source/commands/about.js
@@ -12,7 +12,7 @@ module.exports = {
       .setThumbnail(yuta.user.displayAvatarURL())
       .setColor('LuminousVividPink')
       .setDescription(`Hello there, I'm ${yuta.user.username}! I'm a cute lil music bot!`)
-      .addFields({ name: 'More about me', value: `>>> **• Developer**: ${yuta.users.cache.get(devId)}\n**• Birth**: <t:${Math.round(yuta.user.createdTimestamp/1000)}:F>\n**• Written in**: Node.js, Discord.js\n`});
+      .addFields({ name: 'More about me', value: `>>> * Developer: ${yuta.users.cache.get(devId)}\n* Birth: <t:${Math.round(yuta.user.createdTimestamp/1000)}:F>\n* Written in: Node.js, Discord.js\n`});
     interaction.reply({ embeds: [infoEmb] });
   },
 };
