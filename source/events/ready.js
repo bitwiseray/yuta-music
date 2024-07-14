ec7f410dea5a31970061b265d0b5363a94b89ab6
Ray <121376297+bitwiseray@users.noreply.github.com>
2024-07-14 19:21:16 +0000

    removed status change on ready event

--- source/events/ready.js
+++ source/events/ready.js
@@ -5,13 +5,5 @@ module.exports = {
   once: true,
   execute(yuta) {
     console.log(`Ready! logged has ${yuta.user.username}!`);
-    let status = `Tipsy by Wanuka`;
-    yuta.user.setPresence({
-      activities: [{
-        name: status,
-        type: ActivityType.Listening
-      }],
-      status: 'online'
-    });
   },
 };
